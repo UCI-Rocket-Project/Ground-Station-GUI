50e7ca6a1a481cf1aba6064e6ac7550cc5d29e73
MichaelKrinsky <34221696+MichaelKrinsky@users.noreply.github.com>
2024-01-10 18:01:08 -0800

    Updated radio service

--- radio_service.py
+++ radio_service.py
@@ -1,5 +1,7 @@
 from threading import Thread, Lock
 import time
+import math
+import random
 
 class LoopThread:
     ''' Basic Loop thread to spin off a thread and run a function any number of times for any given interval'''
@@ -15,6 +17,7 @@ class LoopThread:
                 self.func()
                 time.sleep(sleep_time)
         self.t = Thread(target=threading_func)
+        self.t.daemon = True
         self.t.start()
         
     def stop(self):
@@ -25,15 +28,31 @@ class LoopThread:
 class ECUConnection:
     def __init__(self):
         self.lock = Lock()
-        
+        self.data = {
+            "pt": [0,0,0],
+            "tc":[0,0,0],
+            "solenoid":[0,0,0]
+        }
+        def rando_change():
+            key = ["pt","tc","solenoid"][random.randint(0,2)]
+            index = random.randint(0,2)
+            val = random.randint(0,2)
+            self.set_data(key,index,val)
+        self.t = LoopThread(rando_change)
+        self.t.loop(5)
+
     def get_data(self):
         with self.lock:
+            return self.data
+        
+    def set_data(self, key, index, val):
+        print(key,index,val)
+        with self.lock:
+            self.data[key][index] = val
 
 
 if __name__ == "__main__":
-    def just_print():
-        print("print")
-    l = LoopThread(just_print)
-    l.loop(2)
-    time.sleep(5)
-    l.stop()
+   ecu = ECUConnection()
+   while True:
+       print(ecu.get_data())
+       time.sleep(0.5)
